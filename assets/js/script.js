a0c579b4d9aa26513b8cf70dde2be145cebb7b93
LAgrl <lizethariasc14@gmail.com>
2021-05-06 22:25:06 -0700

    save tasks from localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,8 +43,7 @@ var createTaskEl = function(taskDataObj) {
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.setAttribute("data-task-id", taskIdCounter);  
-  console.log(taskDataObj);
-  console.log(taskDataObj.status);
+ 
 
   var taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
@@ -52,6 +51,8 @@ var createTaskEl = function(taskDataObj) {
   listItemEl.appendChild(taskInfoEl);
   taskDataObj.id = taskIdCounter;
   tasks.push(taskDataObj);
+
+  saveTasks();
 
   
   // create task actions (buttons and select) for task
@@ -118,6 +119,8 @@ var completeEditTask = function(taskName, taskType, taskId) {
       }
   };
 
+  saveTasks();
+
   alert("Task Updated!");
 
   // remove data attribute from form
@@ -142,7 +145,7 @@ var taskButtonHandler = function(event) {
 };
 
 var taskStatusChangeHandler = function(event) {
-  console.log(event.target.value);
+
 
   // find task list item based on event.target's data-task-id attribute
   var taskId = event.target.getAttribute("data-task-id");
@@ -167,6 +170,7 @@ var taskStatusChangeHandler = function(event) {
           console.log(tasks);
       }
   }
+  saveTasks();
 };
 
 var editTask = function(taskId) {
@@ -211,7 +215,13 @@ var deleteTask = function(taskId) {
 
   // reassign tasks array to be the same as updatedTaskArr
   tasks = updatedTaskArr;
+
+  saveTasks();
 };
+
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
